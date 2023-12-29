928119a9cd9cd61a4e90eec493414e8274c38efd
Shane Celis <shane.celis@gmail.com>
2023-12-29 04:45:26 -0500

    feature: Show error handling in number eg.

--- examples/number.rs
+++ examples/number.rs
@@ -1,9 +1,16 @@
 use asky::Number;
 
 fn main() -> std::io::Result<()> {
-    if let Ok(age) = Number::<u8>::new("How old are you?").prompt()? {
+    let mut result;
+    loop {
+        result = Number::<u8>::new("How old are you?").prompt()?;
+        if result.is_ok() {
+            break;
+        }
+    }
+    if let Ok(age) = result {
         if age <= 60 {
-            println!("Pretty young");
+            println!("Pretty young.");
         }
     }
 
